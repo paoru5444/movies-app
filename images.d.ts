3b739de9ffa077ecd73ee1bd91332146e50c85e6
Paulo Roberto <paulo.pereira.dev@gmail.com>
2026-03-25 22:06:03 -0300

    feat: add project image assets

---
+++ images.d.ts
@@ -0,0 +1,4 @@
+declare module '*.png' {
+  const value: ImageSourcePropType | undefined;
+  export default value;
+}
---
+++ src/constants/images.ts
@@ -0,0 +1,9 @@
+import movieNotFound from '../../assets/images/movie-not-found.png';
+import noMovieSaved from '../../assets/images/no-movie-saved.png';
+import popcorn from '../../assets/images/popcorn.png';
+
+export const images = {
+  movieNotFound,
+  noMovieSaved,
+  popcorn,
+};
