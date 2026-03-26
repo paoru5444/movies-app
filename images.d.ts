9944d7e5b881fa33e7e890345d245ef57e05f5b8
Paulo Roberto <paulo.pereira.dev@gmail.com>
2026-03-25 22:55:57 -0300

    feat: add project icons and splash screen lib

--- images.d.ts
+++ images.d.ts
@@ -2,3 +2,8 @@ declare module '*.png' {
   const value: ImageSourcePropType | undefined;
   export default value;
 }
+
+declare module '*.svg' {
+  const value: ImageSourcePropType | undefined;
+  export default value;
+}
---
+++ src/constants/icons.ts
@@ -0,0 +1,23 @@
+import arrowLeft from '../../assets/icons/arrow-left.svg';
+import bookmark from '../../assets/icons/bookmark.svg';
+import calendarBlank from '../../assets/icons/calendar-blank.svg';
+import clock from '../../assets/icons/clock.svg';
+import home from '../../assets/icons/home.svg';
+import searchLeft from '../../assets/icons/search-left.svg';
+import searchRight from '../../assets/icons/search-right.svg';
+import star from '../../assets/icons/star.svg';
+import ticket from '../../assets/icons/ticket.svg';
+import topBarRight from '../../assets/icons/top-bar-right.svg';
+
+export const icons = {
+  arrowLeft,
+  bookmark,
+  calendarBlank,
+  clock,
+  home,
+  searchLeft,
+  searchRight,
+  star,
+  ticket,
+  topBarRight,
+};
