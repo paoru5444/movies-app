bd1160fadd2b583b63a7592879cffc834cd2b1c6
Paulo Roberto <paulo.pereira.dev@gmail.com>
2026-03-28 14:45:16 -0300

    feat: create axios api instance

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500'
+
 export const api = axios.create({
-  baseURL: ' https://developers.themoviedb.org',
+  baseURL: BASE_URL,
   timeout: 1000,
-  headers: { 'X-Custom-Header': 'foobar' },
+  headers: {
+    'X-Custom-Header': 'foobar',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNjg0MzUzMzUzMjlkYTY3MTc3Nzk2MDA5MTYxYjA2ZCIsIm5iZiI6MTc3NDU3MzA2Ni4wNzIsInN1YiI6IjY5YzVkNjBhMTE2NWUzYTIwNzRkYzdhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MCuaYoqCNoSGnPrsl5zbMFUhNI0FipPp0KT6zmEENcs',
+  },
 });
