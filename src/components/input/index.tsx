f9bdb38d7766d67d579195b860052da3bee2466f
Paulo Roberto <paulo.pereira.dev@gmail.com>
2026-03-28 14:46:20 -0300

    feat: create input component

---
+++ src/components/input/index.tsx
@@ -0,0 +1,51 @@
+import {
+  View,
+  TextInput,
+  TextInputProps,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import React from 'react';
+import { Image } from 'react-native-svg';
+import { colors } from '../../constants/colors';
+
+interface InputProps extends TextInputProps {
+  icon: any;
+}
+
+export default function Input({ icon: Icon, ...rest }: InputProps) {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholderTextColor={colors.gray}
+        {...rest}
+      />
+
+      {Icon && (
+        <TouchableOpacity>
+          <Icon />
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 42,
+    borderRadius: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: colors.darkGray,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: 10,
+  },
+  input: {
+    flex: 1,
+    color: colors.gray,
+  },
+});
