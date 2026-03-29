be1ee806c683523c35500cdfd0fff196b34ff23c
Paulo Roberto <paulo.pereira.dev@gmail.com>
2026-03-29 11:03:59 -0300

    feat: update header component to bookmark

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,52 +4,58 @@ import {
   TouchableOpacity,
   StyleProp,
   ViewStyle,
+  StyleSheet,
 } from 'react-native';
 import React from 'react';
 import { colors } from '@/src/constants/colors';
-import { FilterImage } from 'react-native-svg/filter-image';
 import ArrowLeft from '@/assets/icons/arrow-left.svg';
+import TopBarRight from '@/assets/icons/top-bar-right.svg';
+import Bookmark from '@/assets/icons/bookmark.svg';
+
 import { useNavigation } from '@react-navigation/native';
 
 interface HeaderProps {
   pageTitle: string;
   hasBookmark?: boolean;
+  onPressBookmark?: () => void;
   containerStyle?: StyleProp<ViewStyle> | undefined;
+  isBookmarked: boolean;
 }
 
 export default function Header({
   pageTitle,
   hasBookmark,
+  onPressBookmark,
   containerStyle,
+  isBookmarked,
 }: HeaderProps) {
   const navigation = useNavigation();
   return (
-    <View
-      style={[
-        containerStyle,
-        {
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          height: 36,
-        },
-      ]}
-    >
+    <View style={[containerStyle, styles.container]}>
       <TouchableOpacity onPress={() => navigation.goBack()}>
-        <ArrowLeft style={{ width: 20, height: 20 }} />
+        <ArrowLeft style={styles.goback_icon} />
       </TouchableOpacity>
 
-      <Text style={{ fontSize: 16, fontWeight: 600, color: colors.white }}>
-        {pageTitle}
-      </Text>
+      <Text style={styles.pageTitle}>{pageTitle}</Text>
 
       <View>
         {hasBookmark && (
-          <TouchableOpacity>
-            <FilterImage />
+          <TouchableOpacity onPress={onPressBookmark}>
+            {isBookmarked ? <TopBarRight /> : <Bookmark />}
           </TouchableOpacity>
         )}
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 36,
+  },
+  goback_icon: { width: 20, height: 20 },
+  pageTitle: { fontSize: 16, fontWeight: 600, color: colors.white },
+});
